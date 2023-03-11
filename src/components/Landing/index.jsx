91d89712437dc46b669be698a6b02dba50a94f2e
Adnan Ali <adnanali6688@gmail.com>
2023-03-12 00:31:00 +0100

    added products button in landing page

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./Landing.module.scss";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -10,6 +11,7 @@ const Landing = () => {
             <div className={styles.line}></div>
           </div>
           <div className={styles.heading}>Our Priority</div>
+          <button className={styles.btnProducts}>Products</button>
         </div>
         <div className={styles.image}>
           <img src={process.env.PUBLIC_URL + "/media/images/scissor.png"} />
